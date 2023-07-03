ef3041b016282a3a54096ea98c5d958233469c62
Ruslan Lyubimov <ruslan_lyubimov@epam.com>
2023-07-04 00:21:11 +0500

    added Inventory class

--- hw2/src/Consumable.ts
+++ hw2/src/Consumable.ts
@@ -1,20 +1,20 @@
 import { Item } from "./Item";
 export class Consumable extends Item {
+  public isConsumed: boolean;
   private isSpoiled: boolean;
-  private isConsumed: boolean;
 
   constructor(name: string, value: number, weight: number, isSpoiled: boolean) {
     super(name, value, weight);
-    this.isSpoiled = isSpoiled;
     this.isConsumed = false;
+    this.isSpoiled = isSpoiled;
   }
 
   use(): void {
     if (this.isConsumed) {
-      console.log("There is nothing left of the " + this.name + " to consume.");
+      console.log(`There is nothing left of the ${this.name} to consume.`);
     } else {
       this.isConsumed = true;
-      console.log("You consumed the " + this.name + ".");
+      console.log(`You consumed the ${this.name}.`);
       if (this.isSpoiled) {
         console.log("You feel sick.");
       }
--- hw2/src/Inventory.spec.ts
+++ hw2/src/Inventory.spec.ts
@@ -1,61 +1,59 @@
-// // @ts-nocheck
+import { Item } from "./Item";
+import { Inventory } from "./Inventory";
+import { ItemComparator } from "./ItemComparator";
 
-// import { Item } from "./Item";
-// import { Inventory } from "./Inventory";
-// import { ItemComparator } from "./ItemComparator";
+class ItemWithImplementation extends Item {
+  use() {}
+}
 
-// class ItemWithImplementation extends Item {
-//   use() {}
-// }
+describe("Inventory", () => {
+  beforeEach(() => {
+    Item.resetIdCounter();
+  });
 
-// describe("Inventory", () => {
-//   beforeEach(() => {
-//     Item.resetIdCounter();
-//   });
+  it("should add items", () => {
+    const item = new ItemWithImplementation("ring", 2, 1);
+    const inventory = new Inventory();
 
-//   it("should add items", () => {
-//     const item = new ItemWithImplementation("ring", 2, 1);
-//     const inventory = new Inventory();
+    inventory.addItem(item);
 
-//     inventory.addItem(item);
+    expect(inventory.toString()).toEqual("ring - Value: 2.00, Weight: 1.00");
+  });
 
-//     expect(inventory.toString()).toEqual("ring − Value: 2.00, Weight: 1.00");
-//   });
+  describe("sort()", () => {
+    it("should sort items by comparator", () => {
+      const item = new ItemWithImplementation("ring1", 3, 1);
+      const item2 = new ItemWithImplementation("ring2", 4, 2);
+      const inventory = new Inventory();
 
-//   describe("sort()", () => {
-//     it("should sort items by comparator", () => {
-//       const item = new ItemWithImplementation("ring1", 3, 1);
-//       const item2 = new ItemWithImplementation("ring2", 4, 2);
-//       const inventory = new Inventory();
+      inventory.addItem(item);
+      inventory.addItem(item2);
 
-//       inventory.addItem(item);
-//       inventory.addItem(item2);
+      const comparator: ItemComparator = {
+        compare: () => -1,
+      };
+      const compareSpy = spyOn(comparator, "compare");
 
-//       const comparator: ItemComparator = {
-//         compare: () => -1,
-//       };
-//       const compareSpy = spyOn(comparator, "compare");
+      inventory.sort(comparator);
 
-//       inventory.sort(comparator);
+      expect(compareSpy).toHaveBeenCalledWith(
+        { id: 2, name: "ring2", value: 4, weight: 2 },
+        { id: 1, name: "ring1", value: 3, weight: 1 }
+      );
 
-//       expect(compareSpy).toHaveBeenCalledWith(
-//         { id: 2, name: "ring2", value: 4, weight: 2 },
-//         { id: 1, name: "ring1", value: 3, weight: 1 }
-//       );
+      expect(inventory.toString()).toEqual("ring1 - Value: 3.00, Weight: 1.00, ring2 - Value: 4.00, Weight: 2.00");
+    });
 
-//       expect(inventory.toString()).toEqual("ring1 − Value: 3.00, Weight: 1.00, ring2 − Value: 4.00, Weight: 2.00");
-//     });
+    it("should sort items by value", () => {
+      const item1 = new ItemWithImplementation("ring1", 4, 2);
+      const item2 = new ItemWithImplementation("ring2", 3, 1);
+      const inventory = new Inventory();
 
-//     it("should sort items by value", () => {
-//       const item1 = new ItemWithImplementation("ring1", 4, 2);
-//       const item2 = new ItemWithImplementation("ring2", 3, 1);
-//       const inventory = new Inventory();
+      inventory.addItem(item1);
+      inventory.addItem(item2);
+      inventory.sort();
 
-//       inventory.addItem(item1);
-//       inventory.addItem(item2);
-//       inventory.sort();
-
-//       expect(inventory.toString()).toEqual("ring2 − Value: 3.00, Weight: 1.00, ring1 − Value: 4.00, Weight: 2.00");
-//     });
-//   });
-// });
+      expect(inventory.toString()).toEqual("ring2 - Value: 3.00, Weight: 1.00, ring1 - Value: 4.00, Weight: 2.00");
+    });
+  });
+});
